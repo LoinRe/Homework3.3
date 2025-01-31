e11fb73a14279615037b66f87a96f84a968622a5
LoinRe <winard2@mail.ru>
2025-01-31 20:45:34 +0300

    Initial commit

---
+++ src/main/java/Homework/H3W3Database/H3W3DatabaseApplication.java
@@ -0,0 +1,13 @@
+package Homework.H3W3Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H3W3DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H3W3DatabaseApplication.class, args);
+	}
+
+}
